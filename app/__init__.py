6fca5cc6379b396927c5e2dabbd5df1dd89b112a
Andrezinrc <andrem.33333@gmail.com>
2024-05-01 19:43:52 -0300

    deixando caminho fixo

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def baixar_video():
     try:
         url = request.args.get("url")
         #seu caminho onde deseja salvar
-        caminho_salvar = "/storage/emulated/0/Download/meu_pytube_video"
+        caminho_salvar = "/storage/emulated/0/Download"
         
         if request.args.get("download_iniciado"):
             if url:
@@ -39,7 +39,7 @@ def baixar_musica():
     download_sucesso = False
     try:
         url = request.args.get("url")
-        caminho_salvar = "/storage/emulated/0/Download/meu_pytube_musica"
+        caminho_salvar = "/storage/emulated/0/Download"
         
         if url:
             yt = YouTube(url)
@@ -66,19 +66,19 @@ def baixar_musica():
 
 @app.route("/ver_videos")
 def ver_videos():
-    caminho_video = "/storage/emulated/0/Download/meu_pytube_video"
+    caminho_video = "/storage/emulated/0/Download"
     videos = [f for f in os.listdir(caminho_video) if f.endswith('.mp4')]
     return render_template("ver_videos.html", videos=videos, caminho_video=caminho_video)
 
 @app.route('/ver_videos/<video_nome>')
 def reproduzir_video(video_nome):
-    caminho_video = "/storage/emulated/0/Download/meu_pytube_video"
+    caminho_video = "/storage/emulated/0/Download"
     return send_from_directory(caminho_video, video_nome)
 
 
 @app.route("/ver_musicas")
 def ver_musicas():
-    caminho_musica = "/storage/emulated/0/Download/meu_pytube_musica"
+    caminho_musica = "/storage/emulated/0/Download"
     musicas = [f for f in os.listdir(caminho_musica) if f.endswith('.mp3')]
     for musica in musicas:
         print("musica: ",musica)
@@ -87,7 +87,7 @@ def ver_musicas():
 
 @app.route("/ver_musicas/<musica_nome>")
 def reproduzir_musica(musica_nome):
-    caminho_musica = "/storage/emulated/0/Download/meu_pytube_musica"
+    caminho_musica = "/storage/emulated/0/Download"
     return send_from_directory(caminho_musica, musica_nome)
 
 
