2c3b4cb183595493f63df6c2f52c1afc9b3132d0
Andrezinrc <andrem.33333@gmail.com>
2024-05-01 07:04:01 -0300

    arruma o esttilo da aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -6,28 +6,27 @@ import os
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "euamoprogramar"
 
-caminho = "/storage/emulated/0"
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/baixar_video", methods=["GET"])
 def baixar_video():
     download_sucesso = False
     try:
-        pasta = request.args.get("pasta")
         url = request.args.get("url")
-
-        caminho_salvar = caminho + ("/" + pasta if pasta else "")
-        if url:
-            yt = YouTube(url)
-            print(f"Iniciando download: {yt.title}")
-            stream = yt.streams.get_highest_resolution()
-            stream.download(caminho_salvar)
-            download_sucesso = True
-            flash("Download feito com sucesso")
-            flash(yt.title)
+        #seu caminho onde deseja salvar
+        caminho_salvar = "/storage/emulated/0/Download/meu_pytube_video"
+        
+        if request.args.get("download_iniciado"):
+            if url:
+                yt = YouTube(url)
+                print(f"Iniciando download: {yt.title}")
+                stream = yt.streams.get_highest_resolution()
+                stream.download(caminho_salvar)
+                download_sucesso = True
+                flash(f"Download de {yt.title} feito com sucesso")
     except Exception as erro:
         flash("Houve um erro ao baixar")
         print("Houve um erro: ", erro)
@@ -39,52 +38,58 @@ def baixar_video():
 def baixar_musica():
     download_sucesso = False
     try:
-        pasta = request.args.get("pasta")
         url = request.args.get("url")
-
-        caminho_salvar = caminho + ("/" + pasta if pasta else "/meu_pytube_musica")
-
+        caminho_salvar = "/storage/emulated/0/Download/meu_pytube_musica"
+        
         if url:
             yt = YouTube(url)
             print(f"Iniciando download: {yt.title}")
-            flash("Audio: ",yt.title)
-            stream_video = yt.streams.get_highest_resolution()
-            caminho_video = stream_video.download(output_path=caminho_salvar)
-            titulo_video = yt.title
-            caminho_audio = f"{caminho_salvar}/{titulo_video}.mp3"
+            
+            stream_audio = yt.streams.filter(only_audio=True).first()
+            caminho_video = stream_audio.download(output_path=caminho_salvar)
+            
+            # Converter o áudio para MP3
+            caminho_audio = f"{caminho_salvar}/{yt.title}.mp3"
             clip_video = AudioSegment.from_file(caminho_video)
             clip_video.export(caminho_audio, format="mp3")
+            
+            os.remove(caminho_video)
+            
             download_sucesso = True
-            flash("Download feito com sucesso")
+            flash(f"Download de {yt.title} feito com sucesso")
     except Exception as erro:
         flash("Houve um erro ao baixar")
-        print("Houve um erro: ", erro)
+        print("Houve um erro:", erro)
 
     return render_template("baixar_musica.html", download_sucesso=download_sucesso)
 
+
 @app.route("/ver_videos")
 def ver_videos():
-    caminho_video = caminho + "/Download/meu_pytube_video"
+    caminho_video = "/storage/emulated/0/Download/meu_pytube_video"
     videos = [f for f in os.listdir(caminho_video) if f.endswith('.mp4')]
     return render_template("ver_videos.html", videos=videos, caminho_video=caminho_video)
 
 @app.route('/ver_videos/<video_nome>')
 def reproduzir_video(video_nome):
-    caminho_video = caminho + "/Download/meu_pytube_video"
+    caminho_video = "/storage/emulated/0/Download/meu_pytube_video"
     return send_from_directory(caminho_video, video_nome)
+
 
 @app.route("/ver_musicas")
 def ver_musicas():
-    caminho_musica = caminho + "/Download/meu_pytube_musica"
+    caminho_musica = "/storage/emulated/0/Download/meu_pytube_musica"
     musicas = [f for f in os.listdir(caminho_musica) if f.endswith('.mp3')]
     for musica in musicas:
         print("musica: ",musica)
     return render_template("ver_musicas.html", musicas=musicas, caminho_musica=caminho_musica)
 
+
 @app.route("/ver_musicas/<musica_nome>")
 def reproduzir_musica(musica_nome):
-    caminho_musica = caminho + "/Download/meu_pytube_musica"
+    caminho_musica = "/storage/emulated/0/Download/meu_pytube_musica"
     return send_from_directory(caminho_musica, musica_nome)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
